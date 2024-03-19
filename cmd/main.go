1725e76821c0fa35de6286ed20ee36caf69b6a4f
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2024-03-19 11:37:13 -0300

    feat: pipeline pattern

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,114 @@
 package main
 
 import (
-	processimage "process-image/pkg/process-image"
+	"fmt"
+	"image"
 	"strings"
 	"sync"
+	"time"
+
+	processimage "process-image/internal/process-image"
 )
 
-var paths = []string{
-	"images/1.jpeg",
-	"images/2.jpeg",
-	"images/3.jpg",
+type Job struct {
+	InputPath  string
+	OutputPath string
+	Image      image.Image
+}
+
+type ReadJobChan <-chan Job
+
+type ImagePaths []string
+
+func loadImage(paths ImagePaths) ReadJobChan {
+	out := make(chan Job)
+
+	go func() {
+		var wg sync.WaitGroup
+
+		wg.Add(len(paths))
+		for _, path := range paths {
+			go func(path string) {
+				defer wg.Done()
+				job := Job{
+					InputPath:  path,
+					OutputPath: strings.Replace(path, "images/", "images/output/", 1),
+					Image:      processimage.Read(path),
+				}
+				out <- job
+			}(path)
+		}
+		wg.Wait()
+
+		close(out)
+	}()
+
+	return out
+}
+
+func resizeImage(input ReadJobChan) ReadJobChan {
+	out := make(chan Job)
+
+	go func() {
+		for job := range input {
+			job.Image = processimage.Resize(job.Image, 200, 200)
+			out <- job
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func grayscaleImage(input ReadJobChan) ReadJobChan {
+	out := make(chan Job)
+
+	go func() {
+		for job := range input {
+			job.Image = processimage.Grayscale(job.Image)
+			out <- job
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func saveImage(input ReadJobChan) <-chan bool {
+	out := make(chan bool)
+
+	go func() {
+		for job := range input {
+			processimage.Write(job.OutputPath, job.Image)
+			out <- true
+		}
+		close(out)
+	}()
+
+	return out
 }
 
 func main() {
-	var wg sync.WaitGroup
-
-	wg.Add(len(paths))
-	for _, path := range paths {
-		go func(path string) {
-			defer wg.Done()
-			img := processimage.Read(path)
-			resizedImg := processimage.Resize(img)
-			processimage.Write(strings.Replace(path, "images/", "images/output/", 1), resizedImg)
-		}(path)
+	paths := ImagePaths{
+		"images/1.jpeg",
+		"images/2.jpeg",
+		"images/3.jpg",
 	}
 
-	wg.Wait()
-	println("done")
+	now := time.Now()
+
+	c1 := loadImage(paths)
+	c2 := resizeImage(c1)
+	c3 := grayscaleImage(c2)
+
+	for success := range saveImage(c3) {
+		if success {
+			fmt.Println("Success")
+		} else {
+
+			fmt.Println("Failed")
+		}
+	}
+
+	fmt.Println("done", time.Since(now))
 }
